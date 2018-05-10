4be8f6b269e189e3df7afc52cbe1f3dc3cff2ab1
Nye Smith <nyedess@gmail.com>
2018-05-10 02:08:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function happyHolidayTo(holiday, name){
   return "Happy "+ holiday +", " + name +"!" 
 }
 function holidayCountdown (holiday, days){
-  return "It's "+days+ "days until " + holiday+ "!"
+  return "It's "+days+ " days until " + holiday+ "!"
 }
