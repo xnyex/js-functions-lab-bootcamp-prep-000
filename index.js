d5341f951771a626df8194cad2aee917f526906f
Nye Smith <nyedess@gmail.com>
2018-05-10 02:04:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function happyHolidaysTo(name){
   return "Happy holidays, " + name+"!";
 }
 function happyHolidayTo(holiday, name){
-  return "Happy "+ holiday + name +"!" 
+  return "Happy "+ holiday +", " + name +"!" 
 }
